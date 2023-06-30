526ed1893772e3db0cacebc245d5b2930adbd318
wangxp01 <flywangle@hotmail.com>
2023-06-30 15:33:03 +0800

    修改IDO统计表为所有IDO

--- IDO/ido_statis.py
+++ IDO/ido_statis.py
@@ -35,19 +35,15 @@ def accessOracle(query):
 
 
 def main():
-
-    zone = [3101, 3104, 3107, 3108, 3201, 3202, 3203, 3204, 3241, 3242, 3243, 3244, 3143, 3144, 3111, 3113, 3114, 3119, 3120, 3149, 3150]
-    for area in zone:
-        # anothercursor = Database(dbname='ics', username='it', password='1111111', host='10.31.9.24', port='3306')
-        ido_list = cursor.run_query("select areaid, zoneid, equipmentid from ics.ido_list where areaid ={}".format(area))
-        for ido in ido_list:
-            sqlquery = "SELECT EVENTTS, AREAID,ZONEID,EQUIPMENTID,STATISTICALID,VALUE FROM  WC_STATISTICALDATA  WHERE AREAID = {} and zoneid = {} and EQUIPMENTID = {} and EVENTTS  > TRUNC(SYSDATE-1) and EVENTTS  < TRUNC(SYSDATE) ".format(ido[0], ido[1], ido[2])
-            data = accessOracle(sqlquery)
-            for row in data:
-                orignal_sqlquery = "insert into ics.ido_statis (EVENTTS, AREAID,ZONEID,EQUIPMENTID,STATISTICALID,VALUE)  values ('{}',{},{},{},{},{})".format((row[0]+datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S.%f"), row[1], row[2], row[3], row[4], row[5])
-                optimizal_sqlquery = orignal_sqlquery.replace("'None'", "Null")
-                logging.info(optimizal_sqlquery)
-                cursor.run_query(optimizal_sqlquery)
+    ido_list = cursor.run_query("select areaid, zoneid, equipmentid from ics.ido_list")
+    for ido in ido_list:
+        sqlquery = "SELECT EVENTTS, AREAID,ZONEID,EQUIPMENTID,STATISTICALID,VALUE FROM  WC_STATISTICALDATA  WHERE AREAID = {} and zoneid = {} and EQUIPMENTID = {} and EVENTTS  > TRUNC(SYSDATE-1) and EVENTTS  < TRUNC(SYSDATE) ".format(ido[0], ido[1], ido[2])
+        data = accessOracle(sqlquery)
+        for row in data:
+            orignal_sqlquery = "insert into ics.ido_statis (EVENTTS, AREAID,ZONEID,EQUIPMENTID,STATISTICALID,VALUE)  values ('{}',{},{},{},{},{})".format((row[0] + datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S.%f"), row[1], row[2], row[3], row[4], row[5])
+            optimizal_sqlquery = orignal_sqlquery.replace("'None'", "Null")
+            logging.info(optimizal_sqlquery)
+            cursor.run_query(optimizal_sqlquery)
 
 
 if __name__ == '__main__':
